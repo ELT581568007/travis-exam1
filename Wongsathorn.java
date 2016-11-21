e43718ffeea4151702994ca8a97252a47767ca44
ELT581568007 <wongsathorn.yin@crru.ac.th>
2016-11-21 13:23:49 +0700

    wongsathorn

---
+++ Wongsathorn.java
@@ -0,0 +1,7 @@
+public class Wongsathorn {
+	public static void main(String args[]) {
+		System.out.println("Wongsathorn");
+		System.out.println("Yindee");
+		System.out.println("581568007");
+	}
+}
